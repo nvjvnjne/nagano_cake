cd69e847b160e908f08930d66f9c7f34c6537c98
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2022-12-05 03:11:25 +0000

    ヘッダーを編集中

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  #管理者用
+  #URL /admin/sign_in
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #顧客用
+  #URL /customers/sign_in
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  root to: 'public/homes#top'
+  get '/about'=>'public/homes#about', as: 'about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
