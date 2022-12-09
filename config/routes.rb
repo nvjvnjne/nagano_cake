e5c70fa27dcabcaecda36a8801ffbfb5e6812ec6
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2022-12-09 05:34:53 +0000

    管理者用ログイン画面編集完了

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,10 @@ Rails.application.routes.draw do
 
   #管理者用
   #URL /admin/sign_in
-  namespace :admin do
     devise_for :admins, skip: [:registrations, :passwords], controllers: {
       sessions: "admin/sessions"
     }
     get '/admin'=>'admin/homes#top', as: 'admin_top'
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
