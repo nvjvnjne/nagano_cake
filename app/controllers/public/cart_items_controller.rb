da2ebd728c383a3bea22baa1c619095a4e68dc93
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2022-12-21 02:56:16 +0000

    カート機能実装中 12/21

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,14 +2,13 @@ class Public::CartItemsController < ApplicationController
 
   def create
     cart_item = CartItem.new(cart_item_params)
-    cart_item.user_id = current_customer.id
-    cart_item.item_id = item.id
+    cart_item.customer_id = current_customer.id
     cart_item.save
     redirect_to cart_items_path
   end
 
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
   end
 
   def update
@@ -19,6 +18,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path(cart_item)
   end
 
   def destroy_all
