a6d60f4d5365951bad108dfef0e2dcd382aea733
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2022-12-22 09:28:49 +0000

    注文情報確認画面修正中 12/22

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,7 +3,7 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def create
     order = Order.new(order_params)
     order.customer_id = current_customer.id
@@ -20,13 +20,22 @@ class Public::OrdersController < ApplicationController
 
   def comfirm
     @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items
+    byebug
+    @order.shipping_cost = 800
+    @sum = 0
+    @order.total_payment = current_customer.order.shipping_cost + @sum
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+    render :confirm
   end
 
   def complete
   end
-  
+
   private
-  
+
   def order_params
     params.require(:order).permit(:postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
