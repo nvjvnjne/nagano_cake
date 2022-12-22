197395dff7257451d2f3751faaad687466306ee8
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2022-12-22 13:36:42 +0000

    注文機能80%実装完了 12/22

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -18,16 +18,24 @@ class Public::OrdersController < ApplicationController
   def show
   end
 
-  def comfirm
+  def confirm
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items
-    byebug
     @order.shipping_cost = 800
     @sum = 0
-    @order.total_payment = current_customer.order.shipping_cost + @sum
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.first_name + current_customer.last_name
+    @order.total_payment = @order.shipping_cost + @sum
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else
+
+    end
     render :confirm
   end
 
