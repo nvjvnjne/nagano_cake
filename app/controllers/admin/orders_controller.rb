02d75ea40cc43ec4f6c261e1d0f7e990e0874c57
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2022-12-25 09:12:16 +0000

    管理者注文履歴詳細制作完了 12/25

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,12 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def update
+    order = Order.find(params[:id])
+    order.update
+    redirect_to admin_order_path
   end
 end
