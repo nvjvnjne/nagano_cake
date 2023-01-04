2903ce20079981447404880e729fba4c603c2ff1
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2023-01-04 06:53:32 +0000

    注文ステータス自動更新機能実装完了 1/4

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,19 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     order_detail = OrderDetail.find(params[:id])
-    order_detail.update(order_detail_params)
-    redirect_to admin_order_path
+    order = order_detail.order
+    order_details = order.order_details
+    is_updated = true
+    if order_detail.update(order_detail_params)
+      order.update(status: 2) if order_detail.making_status == "making"
+      order_details.each do |order_detail|
+        if order_detail.making_status != "have_made"
+          is_updated = false
+        end
+      end
+      order.update(status: 3) if is_updated
+    end
+    redirect_to admin_order_path(order)
   end
 
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,13 +2,15 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    
   end
 
   def update
     order = Order.find(params[:id])
-    order.update(order_params)
-    redirect_to admin_order_path
+    order_details = OrderDetail.where(order_id: params[:id])
+    if order.update(order_params)
+      order_details.update_all(making_status: 1) if order.status == "have_paid"
+    end
+      redirect_to admin_order_path
   end
 
   private
